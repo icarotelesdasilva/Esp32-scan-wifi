093ae1212ada1857c6ff722f986648b47e5040d6
Rapazeutolouco <telesicaro04@gmail.com>
2026-05-21 17:32:56 -0300

    ESP-32-Scan.ino

--- ESP-32-Scan.C
+++ ESP-32-Scan.C
@@ -1,1 +1,11 @@
+void setup() {
 
+Serial.begin(115200);
+Serial.printf("feito por icr\n");
+WiFi.mode(WIFI_STA);
+WiFi.disconnect();
+int n = WiFi.scanNetworks();
+ 
+Serial.printf("scan feito: %d\n", n);
+
+}
