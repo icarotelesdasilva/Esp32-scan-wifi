0d12120993f7c3b06437ac3eac55d6d7cbdced7e
Rapazeutolouco <telesicaro04@gmail.com>
2026-05-22 20:07:18 -0300

    ESP-32-Scan.C

--- ESP-32-Scan.C
+++ ESP-32-Scan.C
@@ -1,17 +1,17 @@
-#include <WiFi.h> // [!] codigo ainda vai ter mais atualizações.
+#include <WiFi.h>
 
 void setup() {
-
-Serial.begin(115200);
-Serial.printf("feito por icr\n");
-WiFi.mode(WIFI_STA);
-WiFi.disconnect();
-int n = WiFi.scanNetworks();
- 
-Serial.printf("scan feito: %d\n", n);
-
+    Serial.begin(115200);
+    Serial.printf("feito por icr\n");
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    
+    int n = WiFi.scanNetworks();
+    Serial.printf("scan feito: %d redes encontradas\n", n);
 }
 
 void loop() {
-  // [!] codigo corrigido.
+    int n = WiFi.scanNetworks();
+    Serial.printf("scan feito: %d redes encontradas\n", n);
+    delay(5000);
 }
